81148e5dce8c8c2f5854258b0a250e7183958f53
christophbuehler <christoph@cbuehler.li>
2024-05-03 15:47:30 +0200

    merge translation files

--- src/main/java/com/materialidentity/schemaservice/SchemaController.java
+++ src/main/java/com/materialidentity/schemaservice/SchemaController.java
@@ -1,7 +1,10 @@
 package com.materialidentity.schemaservice;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -10,11 +13,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
 import java.io.StringWriter;
-import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
@@ -29,7 +32,6 @@ import org.apache.fop.apps.FOPException;
 import org.apache.fop.apps.FOUserAgent;
 import org.apache.fop.apps.Fop;
 import org.apache.fop.apps.FopFactory;
-import org.apache.fop.apps.FopFactoryConfig;
 import org.apache.fop.apps.MimeConstants;
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -41,6 +43,7 @@ import org.json.JSONObject;
 import org.json.XML;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -55,6 +58,8 @@ import org.xml.sax.SAXException;
 @RestController
 public class SchemaController {
 
+  private static final ObjectMapper objectMapper = new ObjectMapper();
+
   @GetMapping("/")
   public String index() {
     return "Schemas Service";
@@ -64,10 +69,30 @@ public class SchemaController {
   public ResponseEntity<byte[]> render(
     @RequestParam("schemaType") String schemaType,
     @RequestParam("schemaVersion") String schemaVersion,
-    @RequestBody Map<String, Object> jsonMap
+    @RequestBody JsonNode certificate
   ) {
     try {
-      String certificateXml = jsonToXml(jsonMap);
+      PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+      JsonNode translations = objectMapper.createObjectNode();
+
+      String translationPattern = Paths
+        .get("schemas", schemaType, schemaVersion, "translations*.json")
+        .toString();
+
+      Resource[] resources = resolver.getResources(
+        "classpath*:" + translationPattern
+      );
+      for (Resource resource : resources) {
+        System.out.println(resource.getURL());
+
+        InputStream is = resource.getInputStream();
+        JsonNode node = objectMapper.readTree(is);
+        translations = merge(translations, node);
+      }
+
+      ((ObjectNode) certificate).set("Translations", translations);
+
+      String certificateXml = jsonNodeToXml(certificate);
 
       String xsltPath = Paths
         .get("schemas", schemaType, schemaVersion, "stylesheet.xsl")
@@ -81,7 +106,7 @@ public class SchemaController {
       String transformedXml = xsltTransform(certificateXml, xsltSource);
       var pdfBytes = generatePdfFromXslFo(transformedXml);
 
-      String certificateString = jsonToString(jsonMap);
+      String certificateString = jsonToString(certificate);
 
       var pdfBytesWithAttachment = attachFileToPdf(
         pdfBytes,
@@ -104,32 +129,46 @@ public class SchemaController {
     }
   }
 
-  public static String jsonToString(Map<String, Object> jsonMap)
+  public static JsonNode merge(JsonNode mainNode, JsonNode updateNode) {
+    Iterator<String> fieldNames = updateNode.fieldNames();
+    while (fieldNames.hasNext()) {
+      String fieldName = fieldNames.next();
+      JsonNode jsonNode = mainNode.get(fieldName);
+      // if field exists and is an embedded object
+      if (jsonNode != null && jsonNode.isObject()) {
+        merge(jsonNode, updateNode.get(fieldName));
+      } else {
+        if (mainNode instanceof ObjectNode) {
+          JsonNode value = updateNode.get(fieldName);
+          ((ObjectNode) mainNode).set(fieldName, value);
+        }
+      }
+    }
+    return mainNode;
+  }
+
+  public static String jsonToString(JsonNode jsonMap)
     throws JsonProcessingException {
     ObjectMapper objectMapper = new ObjectMapper();
     return objectMapper.writeValueAsString(jsonMap);
   }
 
-  public static String jsonToXml(Map<String, Object> jsonMap) {
-    JSONObject json = new JSONObject(jsonMap);
-    return "<root>" + XML.toString(json) + "</root>";
+  public static String jsonNodeToXml(JsonNode jsonNode) throws IOException {
+    XmlMapper xmlMapper = new XmlMapper();
+    return xmlMapper.writer().withRootName("Root").writeValueAsString(jsonNode);
   }
 
-  public String xsltTransform(String xmlSource, String xsltSource) {
-    try {
-      Source xmlInput = new StreamSource(new StringReader(xmlSource));
-      Source xsltInput = new StreamSource(new StringReader(xsltSource));
-      StringWriter outputWriter = new StringWriter();
+  public String xsltTransform(String xmlSource, String xsltSource)
+    throws TransformerException {
+    Source xmlInput = new StreamSource(new StringReader(xmlSource));
+    Source xsltInput = new StreamSource(new StringReader(xsltSource));
+    StringWriter outputWriter = new StringWriter();
 
-      TransformerFactory factory = new TransformerFactoryImpl();
-      Transformer transformer = factory.newTransformer(xsltInput);
-      transformer.transform(xmlInput, new StreamResult(outputWriter));
+    TransformerFactory factory = new TransformerFactoryImpl();
+    Transformer transformer = factory.newTransformer(xsltInput);
+    transformer.transform(xmlInput, new StreamResult(outputWriter));
 
-      return outputWriter.toString();
-    } catch (Exception e) {
-      e.printStackTrace();
-      return null;
-    }
+    return outputWriter.toString();
   }
 
   public static byte[] attachFileToPdf(
@@ -191,16 +230,6 @@ public class SchemaController {
       copyInputStreamToFile(is, tempFile);
       fopFactory = FopFactory.newInstance(tempFile);
     }
-
-    // try {
-    //   // TODO: just throw the exception
-
-    //   File xconf = xconfResource.getFile();
-    //   fopFactory = FopFactory.newInstance(xconf);
-    // } catch (SAXException | IOException e) {
-    //   e.printStackTrace();
-    //   return null;
-    // }
 
     FOUserAgent foUserAgent = fopFactory.newFOUserAgent();
     try {
