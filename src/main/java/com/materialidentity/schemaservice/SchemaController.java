2430efe8fafdaf6caa01cb99739a754d31b10989
S1SEVEN <s1seven@Andres-MacBook-Pro.local>
2024-05-06 10:29:55 +0200

    feat: validation endpoint

--- src/main/java/com/materialidentity/schemaservice/SchemaController.java
+++ src/main/java/com/materialidentity/schemaservice/SchemaController.java
@@ -3,8 +3,18 @@ package com.materialidentity.schemaservice;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.networknt.schema.InputFormat;
+import com.networknt.schema.JsonSchema;
+import com.networknt.schema.JsonSchemaFactory;
+import com.networknt.schema.SpecVersion;
+import com.networknt.schema.ValidationMessage;
+
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -23,38 +33,35 @@ public class SchemaController {
 
   // @GetMapping("/")
   // public String index() {
-  //   return "Schemas Service";
+  // return "Schemas Service";
   // }
 
   @PostMapping("/render")
   public ResponseEntity<byte[]> render(
-    @RequestParam("schemaType") String schemaType,
-    @RequestParam("schemaVersion") String schemaVersion,
-    @RequestParam("languages") String[] languages,
-    @RequestBody JsonNode certificate
-  ) throws Exception {
+      @RequestParam("schemaType") String schemaType,
+      @RequestParam("schemaVersion") String schemaVersion,
+      @RequestParam("languages") String[] languages,
+      @RequestBody JsonNode certificate) throws Exception {
     String translationsPattern = Paths
-      .get("schemas", schemaType, schemaVersion, "translations*.json")
-      .toString();
+        .get("schemas", schemaType, schemaVersion, "translations*.json")
+        .toString();
 
     String certificateJson = jsonToString(certificate);
 
     String xsltPath = Paths
-      .get("schemas", schemaType, schemaVersion, "stylesheet.xsl")
-      .toString();
+        .get("schemas", schemaType, schemaVersion, "stylesheet.xsl")
+        .toString();
 
     Resource xsltResource = new ClassPathResource(xsltPath);
     String xsltSource = new String(
-      Files.readAllBytes(Paths.get(xsltResource.getURI()))
-    );
+        Files.readAllBytes(Paths.get(xsltResource.getURI())));
 
     byte[] pdfBytes = new PDFBuilder()
-      .withXsltTransformer(new XsltTransformer(xsltSource, certificate))
-      .withTranslations(new TranslationLoader(translationsPattern, languages))
-      .withAttachment(
-        new AttachmentManager(certificateJson, "certificate.json")
-      )
-      .build();
+        .withXsltTransformer(new XsltTransformer(xsltSource, certificate))
+        .withTranslations(new TranslationLoader(translationsPattern, languages))
+        .withAttachment(
+            new AttachmentManager(certificateJson, "certificate.json"))
+        .build();
 
     HttpHeaders headers = new HttpHeaders();
     headers.setContentType(MediaType.APPLICATION_PDF);
@@ -63,9 +70,50 @@ public class SchemaController {
     return new ResponseEntity<>(pdfBytes, headers, HttpStatus.OK);
   }
 
+  @PostMapping("/validate")
+  public ResponseEntity<Boolean> validate(
+      @RequestParam("schemaType") String schemaType,
+      @RequestParam("schemaVersion") String schemaVersion,
+      @RequestBody JsonNode requestBody) {
+    try {
+      ObjectMapper objectMapper = new ObjectMapper();
+      String jsonCertificateString = objectMapper.writeValueAsString(requestBody.get("jsonData"));
+      String jsonSchemaString = objectMapper.writeValueAsString(requestBody.get("jsonSchema"));
+
+      // build schema validator with schema definition
+      JsonSchemaFactory schemaFactory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V202012);
+      JsonSchema schema = schemaFactory.getSchema(jsonSchemaString);
+
+      Set<ValidationMessage> validationResult = schema.validate(jsonCertificateString, InputFormat.JSON,
+          executionContext -> {
+            executionContext.getExecutionConfig().setFormatAssertionsEnabled(true);
+          });
+
+      Boolean isValid = validationResult.isEmpty();
+
+      if (!isValid)
+        for (ValidationMessage result : validationResult) {
+          System.out.println(result.getMessage());
+        }
+
+      HttpHeaders headers = new HttpHeaders();
+      headers.setContentType(MediaType.APPLICATION_JSON);
+
+      Map<String, Object> response = new HashMap<>();
+      response.put("success", isValid);
+
+      return new ResponseEntity<>(validationResult.isEmpty(), HttpStatus.OK);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
   public static String jsonToString(JsonNode jsonMap)
-    throws JsonProcessingException {
+      throws JsonProcessingException {
     ObjectMapper objectMapper = new ObjectMapper();
     return objectMapper.writeValueAsString(jsonMap);
   }
+
 }
