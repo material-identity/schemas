c58db595216c52a7c3068381592b30fa4f72f6a1
S1SEVEN <s1seven@Andres-MacBook-Pro.local>
2024-05-06 10:29:55 +0200

    feat: validation endpoint

--- render-demo.js
+++ render-demo.js
@@ -3,20 +3,45 @@ const fs = require('fs');
 const path = require('path');
 
 const filePath = path.join(__dirname, 'src', 'main', 'resources', 'schemas', 'CoA', 'v1.1.0', 'valid-cert.json');
-const json_data = require(filePath);
+const schemaPath = path.join(__dirname, 'src', 'main', 'resources', 'schemas', 'CoA', 'v1.1.0', 'schema.json');
+const jsonData = require(filePath);
+const jsonSchema = require(schemaPath);
 
-const url = 'http://localhost:8080/render';
+const merged_json_data_schema = {
+    jsonData,
+    jsonSchema
+}
+
+
+// const url = 'http://localhost:8080/render';
+const url = 'http://localhost:8080/api/validate';
 // const url = 'https://schema-service-4451088b7fea.herokuapp.com/render';
 
-axios.post(url, json_data, {
-    responseType: 'arraybuffer',
-    params: { schemaType: 'CoA', schemaVersion: 'v1.1.0', languages: 'CN, EN' },
+// axios.post(url, json_data, {
+//     responseType: 'arraybuffer',
+//     params: { schemaType: 'CoA', schemaVersion: 'v1.1.0', languages: 'CN, EN' },
+// })
+//     .then(response => {
+//         if (response.status === 200) {
+//             fs.writeFileSync('output.pdf', response.data, 'binary');
+//             console.log(response.data);
+//             console.log("PDF saved as 'output.pdf'.");
+//         } else {
+//             console.log('Error:', response.status);
+//         }
+//     })
+//     .catch(error => {
+//         console.error('Error:', error.message);
+//     });
+
+
+axios.post(url, merged_json_data_schema, {
+    params: { schemaType: 'CoA', schemaVersion: 'v1.1.0' },
 })
     .then(response => {
         if (response.status === 200) {
-            fs.writeFileSync('output.pdf', response.data, 'binary');
-            console.log(response.data);
-            console.log("PDF saved as 'output.pdf'.");
+            const isSuccess = response.data;
+            console.log('Success:', isSuccess);
         } else {
             console.log('Error:', response.status);
         }
