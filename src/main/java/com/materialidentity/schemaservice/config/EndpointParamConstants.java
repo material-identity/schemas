de1d38996095d2035d6d17ebe7e7794067cf2541
S1SEVEN <s1seven@Andres-MacBook-Pro.local>
2024-05-08 23:23:28 +0200

    feat: added config package and separated constants

---
+++ src/main/java/com/materialidentity/schemaservice/config/EndpointParamConstants.java
@@ -0,0 +1,12 @@
+package com.materialidentity.schemaservice.config;
+
+public final class EndpointParamConstants {
+    // endpoint parameters constants
+    public static final String SCHEMA_TYPE_PARAM = "schemaType";
+    public static final String SCHEMA_VERSION_PARAM = "schemaVersion";
+    public static final String LANGUAGES_PARAM = "languages";
+
+    private EndpointParamConstants() {
+    }
+
+}
