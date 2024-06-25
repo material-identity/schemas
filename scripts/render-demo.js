0d7455ff26fb2e1f615d4d0d69a2ef7878930843
Eamon <eamon0989@gmail.com>
2024-06-25 12:13:16 +0200

    refactor: update render-demo.js with try/catch syntax

--- scripts/render-demo.js
+++ scripts/render-demo.js
@@ -5,22 +5,27 @@ const path = require('path');
 const certificatePath = path.join(
   __dirname,
   // "src/main/resources/schemas/EN10168/v0.4.1/valid-cert.json",
-  'src/main/resources/schemas/CoA/v1.1.0/valid-cert.json'
+  '../src/main/resources/schemas/CoA/v1.1.0/valid-cert.json'
 );
 const certificate = fs.readFileSync(certificatePath);
 
 const url = 'http://localhost:8081/api/render';
 // const url = 'https://schema-service-4451088b7fea.herokuapp.com/api/render';
 
-axios
-  .post(url, certificate, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    responseType: 'arraybuffer',
-    params: { schemaType: 'CoA', schemaVersion: 'v1.1.0', languages: 'DE, EN' },
-  })
-  .then((response) => {
+(async () => {
+  try {
+    const response = await axios.post(url, certificate, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      responseType: 'arraybuffer',
+      params: {
+        schemaType: 'CoA',
+        schemaVersion: 'v1.1.0',
+        languages: 'DE, EN',
+      },
+    });
+    
     if (response.status === 200) {
       fs.writeFileSync('output.pdf', response.data, 'binary');
       console.log(response.data);
@@ -28,10 +33,10 @@ axios
     } else {
       console.log('Error:', response.status);
     }
-  })
-  .catch((error) => {
+  } catch (error) {
     console.error('Error:', error.message);
     if (axios.isAxiosError(error)) {
       console.error('Error response:', error.response?.data.toString());
     }
-  });
+  }
+})();
