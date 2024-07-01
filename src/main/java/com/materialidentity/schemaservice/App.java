bc56b9acb9f37a80f58588cd0a4d5d2908c4de9c
Eamon <eamon0989@gmail.com>
2024-07-01 16:03:21 +0200

    feat: add sentry error tracking, get DSN from env variable

--- src/main/java/com/materialidentity/schemaservice/App.java
+++ src/main/java/com/materialidentity/schemaservice/App.java
@@ -1,11 +1,31 @@
 package com.materialidentity.schemaservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.sentry.Sentry;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class App {
+
+  private static final Logger log = LoggerFactory.getLogger(App.class);
+
+  static Environment env;
+
   public static void main(String[] args) {
-    SpringApplication.run(App.class, args);
+
+    ConfigurableApplicationContext applicationContext = SpringApplication.run(App.class, args);
+    Environment env = applicationContext.getBean(Environment.class);
+    String sentryDsn = env.getProperty("sentry.dsn");
+
+    Sentry.init(options -> {
+      options.setDsn(sentryDsn);
+      options.setTracesSampleRate(1.0);
+      // TODO: get from .env variables
+      options.setEnvironment("local");
+    });
   }
 }
