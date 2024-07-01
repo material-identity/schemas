5334209bc477a6104e35dcc1c76744be5bb8a3ea
Eamon <eamon0989@gmail.com>
2024-07-01 16:14:25 +0200

    refactor: get sentry environment from env variables

--- src/main/java/com/materialidentity/schemaservice/App.java
+++ src/main/java/com/materialidentity/schemaservice/App.java
@@ -13,19 +13,17 @@ public class App {
 
   private static final Logger log = LoggerFactory.getLogger(App.class);
 
-  static Environment env;
-
   public static void main(String[] args) {
 
     ConfigurableApplicationContext applicationContext = SpringApplication.run(App.class, args);
     Environment env = applicationContext.getBean(Environment.class);
     String sentryDsn = env.getProperty("sentry.dsn");
+    String environment = env.getProperty("spring.profiles.active");
 
     Sentry.init(options -> {
       options.setDsn(sentryDsn);
       options.setTracesSampleRate(1.0);
-      // TODO: get from .env variables
-      options.setEnvironment("local");
+      options.setEnvironment(environment);
     });
   }
 }
