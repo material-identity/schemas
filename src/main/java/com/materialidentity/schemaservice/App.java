ec912e43fc666428422b81e7615ec0269dbc21f7
Eamon <eamon0989@gmail.com>
2024-07-02 11:47:04 +0200

    refactor: add not null check for sentryDsn

--- src/main/java/com/materialidentity/schemaservice/App.java
+++ src/main/java/com/materialidentity/schemaservice/App.java
@@ -14,16 +14,17 @@ public class App {
   private static final Logger log = LoggerFactory.getLogger(App.class);
 
   public static void main(String[] args) {
-
     ConfigurableApplicationContext applicationContext = SpringApplication.run(App.class, args);
     Environment env = applicationContext.getBean(Environment.class);
     String sentryDsn = env.getProperty("sentry.dsn");
     String environment = env.getProperty("spring.profiles.active");
 
-    Sentry.init(options -> {
-      options.setDsn(sentryDsn);
-      options.setTracesSampleRate(1.0);
-      options.setEnvironment(environment);
-    });
+    if (sentryDsn != null) {
+      Sentry.init(options -> {
+        options.setDsn(sentryDsn);
+        options.setTracesSampleRate(1.0);
+        options.setEnvironment(environment);
+      });
+    }
   }
 }
