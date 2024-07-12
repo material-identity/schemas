dd98458c376f5a93a236a1d7881a56b3fb8482a7
Eamon <eamon0989@gmail.com>
2024-07-12 09:59:56 +0200

    refactor: remove resources before copying from schemas folders

--- copy-resources.sh
+++ copy-resources.sh
@@ -1,6 +1,8 @@
 # This script copies the resources from the schemas directory 
 # to the src/main/resources/schemas directory before the build step
 mkdir -p ./src/main/resources/schemas/
+rm -rf ./src/main/resources/schemas/*
 rsync -avm --include='*/' --include='translations.json' --include='stylesheet.xsl' --exclude='*' ./schemas/ ./src/main/resources/schemas/
 mkdir -p ./src/test/resources/schemas/
+rm -rf ./src/test/resources/schemas/*
 rsync -avm --include='*/' ./fixtures/ ./src/test/resources/schemas/
