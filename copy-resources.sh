a5f146f35d78c1181dc808e8b3a5c0a6ba0b33e3
eamon0989 <eamon0989@gmail.com>
2024-10-23 14:50:01 +0200

    chore: move fixtures to test folder

--- copy-resources.sh
+++ copy-resources.sh
@@ -5,4 +5,4 @@ rm -rf ./src/main/resources/schemas/*
 rsync -avm --include='*/' --include='translations.json' --include='stylesheet.xsl' --include='schema.json' --exclude='*' ./schemas/ ./src/main/resources/schemas/
 mkdir -p ./src/test/resources/schemas/
 rm -rf ./src/test/resources/schemas/*
-rsync -avm --include='*/' ./fixtures/ ./src/test/resources/schemas/
+rsync -avm --include='*/' ./test/fixtures/ ./src/test/resources/schemas/
