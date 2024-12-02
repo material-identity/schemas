0aab1153f31f0997831e9827f580dea9fd49c187
André Fernandes <anfern.pro@gmail.com>
2024-12-02 10:13:24 +0100

    fix: ui issues

--- ui/src/app/form.component.ts
+++ ui/src/app/form.component.ts
@@ -36,53 +36,11 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
       <app-uploader [formControl]="certificateControl"></app-uploader>
     </section>
     <section>
-      <strong class="text-gray-900">Schema</strong>
-      <p class="text-gray-700 mb-4 text-sm">
-        Select a schema for validation. Schemas are maintained by
-        <a class="underline" taget="_blank" href="https://materialidentity.org/"
-          >Material Identity</a
-        >.
-      </p>
-
-      <mat-radio-group
-        class="rounded-md border border-slate-200 block px-2 py-4"
-        [formControl]="schemaControl"
-      >
-        <div class="grid grid-cols-4">
-          @for (schema of visibleSchemaVersions(); track schema[0]) {
-            <div class="flex flex-col">
-              <span
-                class="ml-[var(--mdc-radio-state-layer-size)] text-sm text-gray-700 mb-2"
-                >{{ schema[0] }}</span
-              >
-              @for (version of schema[1]; track version) {
-                <mat-radio-button
-                  class="example-radio-button"
-                  [value]="schema[0] + '.' + version"
-                  >{{ version }}</mat-radio-button
-                >
-              }
-            </div>
-          }
-        </div>
-      </mat-radio-group>
+      <mat-checkbox [formControl]="attachJson">
+        Attach DMP
+      </mat-checkbox> 
     </section>
-    <section>
-      <strong class="text-gray-900">Languages</strong>
-      <p class="text-gray-700 mb-4 text-sm">
-        Select up to two languages for the PDF generation.
-      </p>
-      <div
-        class="grid grid-cols-4 rounded-md border border-slate-200 px-2 py-4"
-      >
-      <mat-checkbox [formControl]="languagesControl['EN']">English</mat-checkbox>
-        <mat-checkbox [formControl]="languagesControl['DE']">German</mat-checkbox>
-        <!-- <mat-checkbox [formControl]="languagesControl['CN']">Chinese</mat-checkbox> -->
-      </div>
-    </section>
-
     <span class="flex-1"></span>
-
     <div class="flex gap-2 justify-start">
       <button mat-stroked-button color="primary" (click)="render()">
         <mat-icon>picture_as_pdf</mat-icon>
@@ -94,48 +52,21 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
 })
 export class FormComponent {
   @HostBinding('class') class = 'flex flex-col gap-8';
-  readonly schemaVersions = signal<[string, string[]][]>([
-    ['CoA', ['v1.1.0']],
-    ['TKR', ['v0.0.4']],
-    ['EN10168', ['v0.4.1']],
-    ['Forestry', ['v0.0.1']],
-    ['ForestrySource', ['v0.0.1']],
-  ]);
-  readonly showAllVersions = signal(true);
-  readonly visibleSchemaVersions = computed<[string, string[]][]>(() =>
-    this.showAllVersions()
-      ? this.schemaVersions()
-      : this.schemaVersions().map(([schema, versions]) => [
-          schema,
-          versions.slice(0, 1),
-        ])
-  );
-
   readonly certificateControl = new FormControl<File | null>(null);
-  readonly schemaControl = new FormControl<string | null>(null);
-  readonly languagesControl: { [key: string]: FormControl<boolean | null> } = {
-    EN: new FormControl(false),
-    DE: new FormControl(false),
-    CN: new FormControl(false),
-  };
-
+  readonly attachJson = new FormControl<boolean>(true);
   private readonly schemaService = inject(SchemaService);
 
   render() {
     const file = this.certificateControl.value;
-    const selectedSchema = this.schemaControl.value;
-    const selectedLanguages = Object.keys(this.languagesControl)
-      .filter((key) => this.languagesControl[key].value)
-      .join(', ');
-    if (!file || !selectedSchema || !selectedLanguages) return;
+    const attachJson = this.attachJson.value ?? false;
+    if (!file) return;
     const reader = new FileReader();
 
     reader.onload = (e: any) => {
       const text = e.target.result; // This is the content of the file as a string
       console.log('File content:', text, JSON.parse(text));
       const certificate = JSON.parse(text);
-      const [schemaType, schemaVersion] = selectedSchema.split(/(?<=^[^.]+)\./);
-      this.schemaService.render(certificate, schemaType, schemaVersion, selectedLanguages);
+      this.schemaService.render(certificate, attachJson);
     };
 
     reader.onerror = () => {
