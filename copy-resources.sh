803265e345bc00bf46847e24875e5a1d1bc0ff84
Aleksandra Markovic <aleksandra.markovic.1812@gmail.com>
2025-03-26 16:53:53 +0100

    fix: run ng build inside ui folder and copy bundled ui

--- copy-resources.sh
+++ copy-resources.sh
@@ -1,6 +1,5 @@
 # This script copies the resources from the schemas directory and from s3bucket
-# to the src/main/resources/schemas directory before the build step.
-# It also builds UI and copies bundled UI files into static folder
+# to the src/main/resources/schemas directory before the build step
 npm ci
 node copy-from-s3bucket.js || { echo "Failed to execute copy-from-s3bucket.js"; }
 mkdir -p ./src/main/resources/schemas/
@@ -10,5 +9,7 @@ mkdir -p ./src/test/resources/schemas/
 rm -rf ./src/test/resources/schemas/*
 rsync -avm --include='*/' --include='*' ./test/fixtures/ ./src/test/resources/schemas/
 # Build UI and copy files
-ng build
-node copy-bundled-ui.js || { echo "Failed to execute copy-bundled-ui.js"; }
+cd ui
+ng build || { echo "UI build failed"; exit 1; }
+cd - > /dev/null || { echo "Failed to return to root directory"; exit 1; }
+node copy-bundled-ui.js || { echo "Failed to execute copy-bundled-ui.js"; exit 1; }
