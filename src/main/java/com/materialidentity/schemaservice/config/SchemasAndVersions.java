e262a7a202f71143a6ce9ddb1593ed326c464cef
Hannes Stiebitzhofer <hannes@s1seven.com>
2025-09-17 23:51:59 +0200

    feat: add ForestrySource and ForestryOutput v1.0.0 support to Java service
    - Add schema type mappings for forestry-source and forestry-output
    - Add ForestrySource and ForestryOutput to SchemaTypes enum
    - Configure supported versions: ForestrySource v0.0.1,v1.0.0 and ForestryOutput v1.0.0
    - Fix version string case: V1.0.0 -> v1.0.0 for ForestrySource

--- src/main/java/com/materialidentity/schemaservice/config/SchemasAndVersions.java
+++ src/main/java/com/materialidentity/schemaservice/config/SchemasAndVersions.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class SchemasAndVersions {
   public enum SchemaTypes {
-    EN10168, CoA, TKR, Forestry, ForestrySource, Metals, Bluemint, HKM
+    EN10168, CoA, TKR, Forestry, ForestrySource, ForestryOutput, Metals, Bluemint, HKM
   }
 
   public static final Map<SchemaTypes, List<String>> supportedSchemas = Map.of(
@@ -13,7 +13,8 @@ public class SchemasAndVersions {
       SchemaTypes.TKR, List.of("v0.0.4"),
       SchemaTypes.EN10168, List.of("v0.4.1", "v0.5.0"),
       SchemaTypes.Forestry, List.of("v0.0.1"),
-      SchemaTypes.ForestrySource, List.of("v0.0.1"),
+      SchemaTypes.ForestrySource, List.of("v0.0.1", "v1.0.0"),
+      SchemaTypes.ForestryOutput, List.of("v1.0.0"),
       SchemaTypes.Metals, List.of("v0.0.1"),
       SchemaTypes.Bluemint, List.of("v1.0.0"),
       SchemaTypes.HKM, List.of("v1.0.0"));
